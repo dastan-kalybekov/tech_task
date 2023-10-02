e95a3cb3557ca042ff2e61c838fd784fec3c6de2
dastan_kalybekov <dakedak670@gmail.com>
2023-10-02 22:40:06 +0600

    send commit

--- geometry_calculation.py
+++ geometry_calculation.py
@@ -1,34 +1,48 @@
 from math import pi, sqrt
 
 
-def area_circle(radius):
-    """
-        Расчитываем площадь круга по радиусу.
-    """
-    return round(pi * radius ** 2, 2)
+class Shape:
+    def area(self):
+        pass
 
 
-def area_triangle(side_1, side_2, side_3):
-    """
-        Расчитываем площадь триугольника по трем сторанам.
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
 
-        :p = полупериметр:
-        :s = площадь:
-        :s_r = квадратный коррень:
-    """
-
-    p = (side_1 + side_2 + side_3) / 2
-    s = p * (p - side_1) * (p - side_2) * (p - side_3)
-    s_r = sqrt(s)
-    return round(s_r, 2)
+    def area(self):
+        """
+            Расчитываем площадь круга по радиусу.
+        """
+        return round(pi * self.radius ** 2, 2)
 
 
-def is_right_triangle(side_1, side_2, side_3):
-    """
-        Проверяем является триугольник прямоугольным.
-    """
+class Triangle(Shape):
+    def __init__(self, side_1, side_2, side_3):
+        self.side_1 = side_1
+        self.side_2 = side_2
+        self.side_3 = side_3
 
-    sides = sorted([side_1, side_2, side_3])
-    if sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2:
-        return True
-    return False
+    def area(self):
+        """
+            Расчитываем площадь триугольника по трем сторанам.
+
+            :p = полупериметр:
+            :s = площадь:
+            :s_r = квадратный коррень:
+        """
+
+        p = (self.side_1 + self.side_2 + self.side_3) / 2
+        s = p * (p - self.side_1) * (p - self.side_2) * (p - self.side_3)
+        s_r = sqrt(s)
+        return round(s_r, 2)
+
+    def is_right_triangle(self):
+        """
+            Проверяем является триугольник прямоугольным.
+        """
+
+        sides = sorted([self.side_1, self.side_2, self.side_3])
+        if sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2:
+            return True
+        return False
--- tests.py
+++ tests.py
@@ -1,16 +1,24 @@
 from unittest import TestCase, main
 
-from geometry_calculation import area_triangle, area_circle, is_right_triangle
+from geometry_calculation import Circle, Triangle
 
 
 class TestFigures(TestCase):
+    radius = 7
+    side_1 = 5
+    side_2 = 7
+    side_3 = 10
+
+    def setUp(self):
+        self.circle = Circle(self.radius)
+        self.triangle = Triangle(self.side_1, self.side_2, self.side_3)
 
     def test_area_circle(self):
         """
                 Тестируем вычисления площади круга.
         """
         excepted_result = 153.94
-        test = area_circle(7)
+        test = self.circle.area()
         self.assertEquals(excepted_result, test)
 
     def test_area_triangle_(self):
@@ -18,15 +26,15 @@ class TestFigures(TestCase):
                     Тестируем вычисления площади триугольника.
         """
         excepted_result = 16.25
-        test = area_triangle(5, 7, 10)
+        test = self.triangle.area()
         self.assertEquals(excepted_result, test)
 
     def test_is_right_triangle(self):
         """
             Тестируем является ли треугольник прямоугольным.
         """
-        excepted_result = True
-        test = is_right_triangle(3, 4, 5)
+        excepted_result = False
+        test = self.triangle.is_right_triangle()
         self.assertEquals(excepted_result, test)
 
 
